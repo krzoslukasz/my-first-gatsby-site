3cbbb1f4b7226fd16c9cfa26f41371de62e17b3a
krzoslukasz <ukaszkrzos@gmail.com>
2022-02-14 22:57:18 +0100

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/abo.js
@@ -0,0 +1,28 @@
+
+import * as React from 'react'
+import Layout from '../components/layout'
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </Layout>
+    )
+}
+export default AboutPage
+
+// import * as React from 'react'
+// import { Link } from 'gatsby'
+//
+//
+// const AboPage = () => {
+//     return (
+//         <main>
+//             <title>Abo Page</title>
+//             <h1>Welcome to my Abo site!</h1>
+//             <Link to="/">About</Link>
+//             <p>I'm making this by following the Gatsby Tutorial.</p>
+//         </main>
+//     )
+// }
+//
+// export default AboPage
